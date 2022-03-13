3111bf01606c9718c386519459842cf7da68b104
Loren Mahia <lmahiam98@gmail.com>
2022-03-13 18:00:25 -0400

    change

--- server/index.js
+++ server/index.js
@@ -1,30 +1,25 @@
 const express = require('express');
 const cors = require('cors');
-//const http = require('http');
 
 const authRoutes = require("./routes/auth.js");
-
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+require('dotenv').config();
 
-app.use(cors({
-    credentials: 'true',
-    optionSuccessStatus:200,
-}));
+
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-    console.log('Hello');
     res.send('Hello, World!');
 });
 
 
 
 app.use('/auth', authRoutes);
-
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
